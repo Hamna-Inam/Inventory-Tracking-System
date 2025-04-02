755738e9daa9f6c1652549aa6c63962bca83ec37
Hamna-Inam <h.abro.27113@khi.iba.edu.pk>
2025-04-02 23:04:51 +0500

    integrated Basic Auth

---
+++ src/middleware/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const basicAuth = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        res.status(401).json({ error: "Unauthorized: Missing Authorization Header" });
+    }
+
+    else if (!authHeader.startsWith("Basic ")) {
+        res.status(401).json({ error: "Unauthorized: Missing Authorization Header" });
+    }
+
+    else {
+        const base64Credentials = authHeader.split(" ")[1]; 
+    const decodedCredentials = Buffer.from(base64Credentials, "base64").toString("utf8");
+    const [username, password] = decodedCredentials.split(":"); 
+
+    const validUsername = process.env.BASIC_AUTH_USERNAME;
+    const validPassword = process.env.BASIC_AUTH_PASSWORD;
+
+    if (username !== validUsername || password !== validPassword ) { 
+        res.status(403).json({ error: "Forbidden: Invalid credentials" });
+  }
+
+  else {
+    
+    next();
+
+  }
+    }
+
+     
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,24 @@
 import express from "express";
 import { AddProduct, AddStock, AddStore, DeleteProduct, DeleteStock, DeleteStore, GetAllProducts, GetAllStock, GetAllStores, GetProductById, GetStore, GetStoreStock, UpdateProduct, UpdateStock, UpdateStore } from "../controllers";
+import { basicAuth } from "../middleware";
 
 const router = express.Router();
 
-router.post("/add-product", AddProduct);
-router.get("/get-products", GetAllProducts);
-router.get("/get-product/:id", GetProductById);
-router.delete("/delete-product/:id",DeleteProduct);
-router.put('/update-product/',UpdateProduct)
-router.post("/add-stock",AddStock);
-router.get("/get-store-stock/:id",GetStoreStock);
-router.put("/update-store-stock",UpdateStock);
-router.get("/get-stock/",GetAllStock);
-router.post("/add-store", AddStore);
-router.get("/get-stores", GetAllStores);
-router.get("/get-store/:id", GetStore);
-router.put("/update-store/:id", UpdateStore);
-router.delete("/delete-store/:id", DeleteStore);
-router.delete("/delete-stock/:id", DeleteStock);
+router.post("/add-product", basicAuth, AddProduct);
+router.get("/get-products",basicAuth,GetAllProducts);
+router.get("/get-product/:id", basicAuth,GetProductById);
+router.delete("/delete-product/:id",basicAuth,DeleteProduct);
+router.put('/update-product/',basicAuth,UpdateProduct)
+router.post("/add-stock",basicAuth,AddStock);
+router.get("/get-store-stock/:id",basicAuth,GetStoreStock);
+router.put("/update-store-stock",basicAuth,UpdateStock);
+router.get("/get-stock/",basicAuth,GetAllStock);
+router.post("/add-store", basicAuth,AddStore);
+router.get("/get-stores", basicAuth,GetAllStores);
+router.get("/get-store/:id", basicAuth,GetStore);
+router.put("/update-store/:id", basicAuth,UpdateStore);
+router.delete("/delete-store/:id",basicAuth, DeleteStore);
+router.delete("/delete-stock/:id",basicAuth, DeleteStock);
 
 
 export default router;
